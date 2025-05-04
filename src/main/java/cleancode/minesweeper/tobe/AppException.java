41cf5ce928a4876e5d0c7dbd07c3766aa2d7b74b
wmw04087 <wmw04087@gmail.com>
2025-05-04 23:08:48 +0900

    객체 지향 패러다임: SRP

--- src/main/java/cleancode/minesweeper/tobe/AppException.java
+++
@@ -1,8 +0,0 @@
-package cleancode.minesweeper.tobe;
-
-public class AppException extends RuntimeException {
-
-    public AppException(final String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/cleancode/minesweeper/tobe/GameException.java
@@ -0,0 +1,8 @@
+package cleancode.minesweeper.tobe;
+
+public class GameException extends RuntimeException {
+
+    public GameException(final String message) {
+        super(message);
+    }
+}
